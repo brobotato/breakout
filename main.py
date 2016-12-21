efc0156f372028678d9e60bc57b9ec2679496db2
BrianUser <brianxu01@gmail.com>
2016-12-21 14:53:22 -0800

    added sound fixed bug when ball hit exact top corners of map sectioned off game code into run() created victory and loss catches created format for making levels you must start game with space tidied up code maybe other fixes?

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ __author__ = 'BrianUser'
 
 import pygame
 import math
-import random
 import time
 
 pygame.init()
@@ -17,10 +16,20 @@ font = pygame.font.Font("resources/vgaoem.fon", 15)
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 clock = pygame.time.Clock()
 crashed = False
+playing = False
+
+pygame.mixer.init()
+bounce = pygame.mixer.Sound("resources/ball_bounce.wav")
+destroy = pygame.mixer.Sound("resources/block_destroy.wav")
+
+gameover = font.render("Game Over!", True, white)
+gameover_rect = gameover.get_rect(center=(display_width / 2, display_height / 2))
+victory = font.render("You Win!", True, white)
+victory_rect = victory.get_rect(center=(display_width / 2, display_height / 2))
 
 
 def collision(a, b, size):
-    """returns true if the two values are within a certain range of each other"""
+    # returns true if the two values are within a certain range of each other
     if math.fabs(a - b) <= size:
         return True
     else:
@@ -28,19 +37,19 @@ def collision(a, b, size):
 
 
 def render(x, y, sprite):
-    """just blit rewritten for convenience"""
+    # just blit rewritten for convenience
     gameDisplay.blit(sprite, (x, y))
 
 
 def move(x, y, angle, speed):
-    """move something at an angle and speed"""
+    # move something at an angle and speed
     x += math.cos(math.radians(angle)) * speed
     y += math.sin(math.radians(angle)) * speed
     return x, y
 
 
 def rot_center(image, angle):
-    """rotate an image while keeping its center and size"""
+    # rotate an image while keeping its center and size
     orig_rect = image.get_rect()
     rot_image = pygame.transform.rotate(image, angle)
     rot_rect = orig_rect.copy()
@@ -48,6 +57,88 @@ def rot_center(image, angle):
     rot_image = rot_image.subsurface(rot_rect).copy()
     return rot_image
 
+
+def reset():
+    # reset all game variables
+    paddle.x = 400
+    paddle.y = 500
+    ball.x = 400
+    ball.y = 400
+    ball.angle = 90
+    ball.side = 0
+    ball.lives = 3
+
+
+def run():
+    # execute game code
+    if blocks == []:
+        gameDisplay.blit(victory, victory_rect)
+        pygame.display.update()
+        playing = False
+        return playing
+    if pygame.key.get_pressed()[pygame.K_LEFT] != 0:
+        if paddle.x > 7:
+            paddle.x -= 7
+    if pygame.key.get_pressed()[pygame.K_RIGHT] != 0:
+        if paddle.x < 793:
+            paddle.x += 7
+    ball.x, ball.y = move(ball.x, ball.y, ball.angle, 8)
+    if collision(paddle.x, ball.x, 32) & collision(paddle.y, ball.y, 16):
+        if (ball.x - paddle.x) == 0:
+            ball.angle = -90
+        else:
+            ball.angle = -90 + ((ball.x - paddle.x) * 90 / 32)
+        bounce.play()
+    if (ball.y <= 0) & (0 <= ball.x <= 800):
+        ball.side = "ud"
+    elif (ball.x <= 0 or ball.x >= 800) & (ball.y >= 0):
+        ball.side = "lr"
+    elif (ball.x <= 0) & (ball.y <= 0):
+        ball.angle = 45
+        bounce.play()
+    elif (ball.x >= 800) & (ball.y <= 0):
+        ball.angle = 135
+        bounce.play()
+    if ball.y > 600:
+        if ball.lives > 0:
+            ball.y = 400
+            ball.x = 400
+            paddle.x = 400
+            ball.angle = 90
+            ball.lives -= 1
+            time.sleep(2)
+        else:
+            gameDisplay.blit(gameover, gameover_rect)
+            pygame.display.update()
+            playing = False
+            return playing
+    for x in range(0, ball.lives, 1):
+        render(x * 24 + 8, 576, ball.sprite.image)
+    render(paddle.x - 32, paddle.y - 8, paddle.sprite.image)
+    render(ball.x - 8, ball.y - 8, ball.sprite.image)
+    for b in range(len(blocks)):
+        if collision(ball.x, blocks[b][0], 16) & collision(ball.y, blocks[b][1], 16):
+            if math.fabs(ball.y - blocks[b][1]) < math.fabs(ball.x - blocks[b][0]):
+                ball.side = "lr"
+                blocks[b][2] = True
+                break
+            else:
+                ball.side = "ud"
+                blocks[b][2] = True
+                break
+    for b in blocks:
+        render(b[0] - 16, b[1] - 16, block.image)
+    for b in blocks:
+        if b[2] == True:
+            destroy.play()
+            blocks.remove(b)
+    if ball.side != 0:
+        if ball.side == "lr":
+            ball.angle = math.copysign(180 - math.fabs(ball.angle), ball.angle)
+        elif ball.side == "ud":
+            ball.angle *= -1
+        bounce.play()
+        ball.side = 0
 
 class paddle:
     x = 400
@@ -66,77 +157,27 @@ class ball:
     sprite.image = pygame.image.load("ball.png")
 
 
-"""block format = [x,y,set to be destroyed]"""
+# block format = [x,y,set to be destroyed]
 blocks = []
+lvl1 = []
 block = pygame.sprite.Sprite()
 block.image = pygame.image.load("block.png")
 for x in range(64, 768, 32):
     for y in range(96, 256, 32):
-        blocks.append([x, y, False])
+        lvl1.append([x, y, False])
+blocks = lvl1
 
 while not crashed:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             crashed = True
-    if pygame.key.get_pressed()[pygame.K_LEFT] != 0:
-        if paddle.x > 8:
-            paddle.x -= 8
-    if pygame.key.get_pressed()[pygame.K_RIGHT] != 0:
-        if paddle.x < 792:
-            paddle.x += 8
-    ball.x, ball.y = move(ball.x, ball.y, ball.angle, 8)
-    if collision(paddle.x, ball.x, 32) & collision(paddle.y, ball.y, 16):
-        if (ball.x - paddle.x) == 0:
-            ball.angle = -90
-        else:
-            ball.angle = -90 + ((ball.x - paddle.x) * 90 / 32)
-    if ball.y < 0:
-        ball.side = "ud"
-    if ball.x < 0 or ball.x > 800:
-        ball.side = "lr"
-    if ball.y > 600:
-        if ball.lives > 0:
-            ball.y = 400
-            ball.x = 400
-            paddle.x = 400
-            ball.angle = 90
-            ball.lives -= 1
-            time.sleep(2)
-        else:
-            crashed = True
-    """ debugging code
-    for b in blocks:
-        score = font.render("{0}, {1}, {2}".format(ball.x, ball.y, ball.angle), True, white)
-    render(10, 10, score)
-    debugging code
-    """
-    for x in range (0,ball.lives,1):
-        render(x*24+8,576,ball.sprite.image)
-    render(paddle.x - 32, paddle.y - 8, paddle.sprite.image)
-    render(ball.x - 8, ball.y - 8, ball.sprite.image)
-    for b in range(len(blocks)):
-        if collision(ball.x, blocks[b][0], 16) & collision(ball.y, blocks[b][1], 16):
-            if math.fabs(ball.y - blocks[b][1]) < math.fabs(ball.x - blocks[b][0]):
-                ball.side = "lr"
-                blocks[b][2] = True
-                break
-            else:
-                ball.side = "ud"
-                blocks[b][2] = True
-                break
-    for b in blocks:
-        render(b[0] - 16, b[1] - 16, block.image)
-    for b in blocks:
-        if b[2] == True:
-            blocks.remove(b)
-    if ball.side == "lr":
-        ball.angle = math.copysign(180 - math.fabs(ball.angle), ball.angle)
-        ball.side = 0
-    if ball.side == "ud":
-        ball.angle *= -1
-        ball.side = 0
-    if blocks == []:
-        crashed = True
+    if (pygame.key.get_pressed()[pygame.K_SPACE] != 0) & (playing == False):
+        reset()
+        playing = True
+    if playing == True:
+        run()
+    else:
+        reset()
     pygame.display.update()
     clock.tick(50)
     gameDisplay.fill(black)
